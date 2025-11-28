522482c3ec656b0a01412147fb70bde58ed6f753
lilyl-3 <lilyllim2@gmail.com>
2025-11-28 13:50:26 -0800

    fixing chat backend structure attempting to fix codacy issues idkkk

--- backend/src/routes/chat.routes.ts
+++ backend/src/routes/chat.routes.ts
@@ -13,22 +13,22 @@ chatRouter.use((req, res, next) => {
 
 // @desc    Get messages for a group
 // @route   GET /api/chat/:groupId/messages
-chatRouter.get('/:groupId/messages', ChatController.getGroupMessages);
+chatRouter.get('/:groupId/messages', (req, res) => ChatController.getGroupMessages(req, res) as Promise<any>);
 
 // @desc    Send a message to a group
 // @route   POST /api/chat/:groupId/message
-chatRouter.post('/:groupId/message', ChatController.sendMessage);
+chatRouter.post('/:groupId/message', (req, res) => ChatController.sendMessage(req, res) as Promise<any>);
 
 // @desc    Create a poll in a group
 // @route   POST /api/chat/:groupId/poll
-chatRouter.post('/:groupId/poll', ChatController.createPoll);
+chatRouter.post('/:groupId/poll', (req, res) => ChatController.createPoll(req, res) as Promise<any>);
 
 // @desc    Vote on a poll
 // @route   POST /api/chat/:groupId/poll/:messageId/vote
-chatRouter.post('/:groupId/poll/:messageId/vote', ChatController.voteOnPoll);
+chatRouter.post('/:groupId/poll/:messageId/vote', (req, res) => ChatController.voteOnPoll(req, res) as Promise<any>);
 
 // @desc    Delete a message
 // @route   DELETE /api/chat/:groupId/message/:messageId
-chatRouter.delete('/:groupId/message/:messageId', ChatController.deleteMessage);
+chatRouter.delete('/:groupId/message/:messageId', (req, res) => ChatController.deleteMessage(req, res) as Promise<any>);
 
 export default chatRouter;
--- backend/src/routes/task.routes.ts
+++ backend/src/routes/task.routes.ts
@@ -13,38 +13,38 @@ taskRouter.use((req, res, next) => {
 
 // @desc    Create a new task
 // @route   POST /api/task
-taskRouter.post('/', TaskController.createTask);
+taskRouter.post('/', (req, res) => TaskController.createTask(req, res) as Promise<any>);
 
 // @desc    Get tasks for current group
 // @route   GET /api/task
-taskRouter.get('/', TaskController.getTasksForGroup);
+taskRouter.get('/', (req, res) => TaskController.getTasksForGroup(req, res) as Promise<any>);
 
 // @desc    Get tasks assigned to current user
 // @route   GET /api/task/my-tasks
-taskRouter.get('/my-tasks', TaskController.getMyTasks);
+taskRouter.get('/my-tasks', (req, res) => TaskController.getMyTasks(req, res) as Promise<any>);
 
 // @desc    Update task status
 // @route   PUT /api/task/:id/status
-taskRouter.put('/:id/status', TaskController.updateTaskStatus);
+taskRouter.put('/:id/status', (req, res) => TaskController.updateTaskStatus(req, res) as Promise<any>);
 
 // @desc    Assign task to users for current week
 // @route   POST /api/task/:id/assign
-taskRouter.post('/:id/assign', TaskController.assignTask);
+taskRouter.post('/:id/assign', (req, res) => TaskController.assignTask(req, res) as Promise<any>);
 
 // @desc    Algorithmically assign all tasks for current week
 // @route   POST /api/task/assign-weekly
-taskRouter.post('/assign-weekly', TaskController.assignWeeklyTasks);
+taskRouter.post('/assign-weekly', (req, res) => TaskController.assignWeeklyTasks(req, res) as Promise<any>);
 
 // @desc    Get tasks for a specific week
 // @route   GET /api/task/week/:weekStart
-taskRouter.get('/week/:weekStart', TaskController.getTasksForWeek);
+taskRouter.get('/week/:weekStart', (req, res) => TaskController.getTasksForWeek(req, res) as Promise<any>);
 
 // @desc    Delete task
 // @route   DELETE /api/task/:id
-taskRouter.delete('/:id', TaskController.deleteTask);
+taskRouter.delete('/:id', (req, res) => TaskController.deleteTask(req, res) as Promise<any>);
 
 // @desc    Get tasks for a specific date
 // @route   GET /api/task/date/:date
-taskRouter.get('/date/:date', TaskController.getTasksForDate);
+taskRouter.get('/date/:date', (req, res) => TaskController.getTasksForDate(req, res) as Promise<any>);
 
 export default taskRouter;
